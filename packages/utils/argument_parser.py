3cbccc1f8177b53bc30877c33420e78a714cd0b1
Jkm07 <jkm070699@gmail.com>
2025-03-31 21:39:18 +0200

    fix: change default params

--- packages/utils/argument_parser.py
+++ packages/utils/argument_parser.py
@@ -5,7 +5,7 @@ def get_train_arguments():
     parser = argparse.ArgumentParser(description="Train main model")
 
     parser.add_argument("--epoch", type=int, default=200)
-    parser.add_argument("--no_improvment_stop", type=int, default=10)
+    parser.add_argument("--no_improvment_stop", type=int, default=3)
 
     parser.add_argument("--train_dir", type=str, default="datasets/amass/run_test_one_sub")
     parser.add_argument("--valid_dir", type=str, default="datasets/amass/run_test")
@@ -16,7 +16,7 @@ def get_train_arguments():
     parser.add_argument("--valid_batch_size", type=int, default=32)
     parser.add_argument("--test_batch_size", type=int, default=32)
 
-    parser.add_argument("--latent_dim", type=int, default=256)
+    parser.add_argument("--latent_dim", type=int, default=2024)
     parser.add_argument("--learning_rate", type=float, default=1e-3)
     parser.add_argument("--sequence_length", type=int, default=60)
 
